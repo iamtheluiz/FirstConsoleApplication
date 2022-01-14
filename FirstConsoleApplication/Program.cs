ed1fcb6826b5f44caac8e709094ded1ed7937a05
iamtheluiz <iamtheluiz.dev@gmail.com>
2022-01-14 11:58:10 -0300

    Adicionar arquivos de projeto.

---
+++ FirstConsoleApplication/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace FirstConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Luiz Gustavo";
+            int age = 23;
+
+            Console.WriteLine("There once was a man named " + name);
+            Console.WriteLine("He was " + age + " years old");
+            Console.WriteLine("He really liked the name " + name);
+            Console.WriteLine("But didn't like being " + age);
+        }
+    }
+}
