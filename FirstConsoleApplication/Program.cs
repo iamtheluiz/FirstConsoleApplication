f0c603346f64873b0a43acf182eba9941fb4fe70
iamtheluiz <iamtheluiz.dev@gmail.com>
2022-01-15 12:22:21 -0300

    pythagorean theorem with user input validation

--- FirstConsoleApplication/Program.cs
+++ FirstConsoleApplication/Program.cs
@@ -12,22 +12,31 @@
             Console.WriteLine("|======================");
 
             double a, b, c;
-            a = readDouble("| -> Altitude (a): ");
-            b = readDouble("| -> Base (b): ");
+            a = ReadDouble("| -> Altitude (a): ");
+            b = ReadDouble("| -> Base (b): ");
 
             c = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
 
             Console.WriteLine("|======================");
             Console.WriteLine("| -> Hypotenuse: " + c);
+            Console.WriteLine("|======================");
 
+            Console.Write("\nPress any key to exit...");
             Console.ReadLine();
         }
 
-        static double readDouble(string p)
+        static double ReadDouble(string message)
         {
-            Console.Write(p);
-            string line = Console.ReadLine();
-            return Convert.ToDouble(line);
+            string line = "";
+            double value;
+
+            while (!double.TryParse(line, out value))
+            {
+                Console.Write(message);    
+                line = Console.ReadLine();
+            }
+
+            return value;
         }
     }
 }
