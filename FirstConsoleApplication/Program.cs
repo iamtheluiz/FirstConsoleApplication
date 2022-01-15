9399df47609b4eb60496e7f22b080a4df7184723
iamtheluiz <iamtheluiz.dev@gmail.com>
2022-01-15 11:33:12 -0300

    pythagorean theorem

--- FirstConsoleApplication/Program.cs
+++ FirstConsoleApplication/Program.cs
@@ -5,36 +5,29 @@
         static void Main(string[] args)
         {
             /**
-             * Working with numbers
+             * Getting user input
              */
-            Console.WriteLine("Addition: " + (8 + 5));
-            Console.WriteLine("Subtraction: " + (8 - 5));
-            Console.WriteLine("Multiplication: " + (8 * 5));
-            Console.WriteLine("Division (int)   : " + (8 / 5));  // Returns int
-            Console.WriteLine("Division (double): " + (8 / 5.0));    // Returns double
-            Console.WriteLine("Modulus: " + (8 % 5));   // Remaining
+            Console.WriteLine("|======================");
+            Console.WriteLine("| Pythagorean Theorem:");
+            Console.WriteLine("|======================");
 
-            Console.WriteLine("\n=========================");
+            double a, b, c;
+            a = readDouble("| -> Altitude (a): ");
+            b = readDouble("| -> Base (b): ");
 
-            int num = 5;
-            Console.WriteLine("Initial Value: " + num);
+            c = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
 
-            // Increment
-            num++;
-            Console.WriteLine("Incremented Value: " + num);
-            // Decrement
-            num--;
-            Console.WriteLine("Decremented Value: " + num);
+            Console.WriteLine("|======================");
+            Console.WriteLine("| -> Hypotenuse: " + c);
 
-            /**
-             * Math methods
-             */
-            Console.WriteLine("\n=========================");
-            Console.WriteLine("Absolute number: " + Math.Abs(-32)); // Absolute value
-            Console.WriteLine("Power: " + Math.Pow(3, 2)); // Power
-            Console.WriteLine("Square root: " + Math.Sqrt(36)); // Square root
-            Console.WriteLine("Max: " + Math.Max(4, 90));   // Get Biggest value
-            Console.WriteLine("Min: " + Math.Min(4, 90));   // Get Smaller value
+            Console.ReadLine();
+        }
+
+        static double readDouble(string p)
+        {
+            Console.Write(p);
+            string line = Console.ReadLine();
+            return Convert.ToDouble(line);
         }
     }
 }
