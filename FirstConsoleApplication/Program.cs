e03a4b62b8e76fcb661ad83bbb94e26a9c0e9428
iamtheluiz <iamtheluiz.dev@gmail.com>
2022-01-16 01:08:19 -0300

    musical note shifter

--- FirstConsoleApplication/Program.cs
+++ FirstConsoleApplication/Program.cs
@@ -5,24 +5,43 @@
         static void Main(string[] args)
         {
             /**
-             * Methods
+             * Tone Shifter
+             * -> User insert a musical note: "A", "A#", "B", "C", ...
+             * -> User insert a value of semitones to shift (allows negative or positive)
+             * -> Return the new note
              */
-            SayHi("Luiz");
-            SayHi("Maria", true);
-            SayHi("Balog");
-            SayHi("Bolize", true);
-            SayHi("Sara", true);
+            string[] notes = { "A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#" };
 
-            Console.ReadLine();
-        }
+            Console.WriteLine("|================================|");
+            Console.WriteLine("|=       Musical Shifter        =|");
+            Console.WriteLine("|================================|");
 
-        static void SayHi(string name, bool hug = false)
-        {
-            Console.Write("Hello "+name+ "!\n");
-            if (hug == true)
+            Console.Write("| - Insert a musical note: ");
+            string note = Console.ReadLine();
+
+            Console.Write("| - Insert a shift value: ");
+            int shift = Convert.ToInt32(Console.ReadLine());
+
+            int newNoteIndex;
+            string newNote;
+
+            if (Array.IndexOf(notes, note) + shift >= 0)
             {
-                Console.WriteLine(" |-> You've been hugged!");
+                newNoteIndex = (Array.IndexOf(notes, note) + shift) % notes.Length;
+                newNote = notes[newNoteIndex];
+            } else
+            {
+                newNoteIndex = Math.Abs((Array.IndexOf(notes, note) + shift + 1) % notes.Length);
+                Array.Reverse(notes);
+                newNote = notes[newNoteIndex];
             }
+
+            Console.WriteLine("|================================|");
+            Console.WriteLine("| New Note: " + newNote);
+            Console.WriteLine("|================================|");
+
+            Console.Write("\nPress ENTER to exit...");
+            Console.ReadLine();
         }
     }
 }
