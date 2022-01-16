ae4b37e8cb4184a551a337534f244dd5645a58f8
iamtheluiz <iamtheluiz.dev@gmail.com>
2022-01-16 15:42:14 -0300

    create guessing game class

---
+++ FirstConsoleApplication/GuessingGame.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstConsoleApplication
+{
+    internal class GuessingGame
+    {
+        readonly Random random = new();
+        private int generatedNumber;
+        private int guessedNumber = 0;
+        private int guessCounter = 0;
+
+        public GuessingGame()
+        {
+            /**
+             * Guess the number
+             * - Generate a number between 1 and 100
+             * - Receive user input with a guess
+             *   > Display if input is greater ou lower than the generated number
+             */
+
+            // Gerenate random number with min and max value
+            generatedNumber = random.Next(1, 100);
+        }
+
+        public void Run()
+        {
+            ShowBanner();
+            while (guessedNumber != generatedNumber)
+            {
+                GetUserGuess();
+                VerifyGuess();
+            }
+        }
+
+        static void ShowBanner()
+        {
+            Console.WriteLine("|==============================|");
+            Console.WriteLine("|=        Guessing Game       =|");
+            Console.WriteLine("|==============================|");
+            Console.WriteLine("| * Try to guess the number ");
+            Console.WriteLine("| between 1 and 100 chosen by");
+            Console.WriteLine("| the computer.");
+            Console.WriteLine("|------------------------------|");
+        }
+
+        private void GetUserGuess()
+        {
+            guessCounter++;
+
+            Console.WriteLine("| Your guess number " + guessCounter);
+            Console.Write("| > ");
+            guessedNumber = Convert.ToInt16(Console.ReadLine());
+        }
+
+        private void VerifyGuess()
+        {
+            if (guessedNumber == generatedNumber)
+            {
+                ShowWin();
+            }
+            else {
+                if (guessedNumber > generatedNumber)
+                {
+                    Console.WriteLine("| * The generated number is lower than " + guessedNumber);
+                }
+                else
+                {
+                    Console.WriteLine("| * The generated number is greater than " + guessedNumber);
+                }
+                Console.WriteLine("|------------------------------|");
+            }
+            
+        }
+
+        private void ShowWin()
+        {
+            Console.WriteLine("| Congratulations! ");
+            Console.WriteLine("| You guessed after " + guessCounter + " attempts.");
+            Console.WriteLine("|==============================|");
+
+            Console.Write("\nPress ENTER to exit...");
+            Console.ReadLine();
+        }
+    }
+}
--- FirstConsoleApplication/Program.cs
+++ FirstConsoleApplication/Program.cs
@@ -4,46 +4,9 @@
     {
         static void Main(string[] args)
         {
-            /**
-             * Gess the number
-             * - Generate a number between 1 and 100
-             * - Receive user input with a guess
-             *   > Display if input is greater ou lower than the generated number
-             */
-            int guessCounter = 0;
-            int guessedNumber = 0;
-            Random random = new Random();
+            GuessingGame game = new GuessingGame();
 
-            // Gerenate random number with min and max value
-            int generatedNumber = random.Next(1, 100);
-
-            Console.WriteLine("|==============================|");
-            Console.WriteLine("|=        Guessing Game       =|");
-            Console.WriteLine("|==============================|");
-            Console.WriteLine("| * Try to guess the number ");
-            Console.WriteLine("| between 1 and 100 chosen by");
-            Console.WriteLine("| the computer.");
-            Console.WriteLine("|------------------------------|");
-            while (guessedNumber != generatedNumber)
-            {
-                guessCounter++;
-
-                Console.WriteLine("| Your guess number " + guessCounter);
-                Console.Write("| > ");
-                guessedNumber = Convert.ToInt16(Console.ReadLine());
-
-                if (guessedNumber < generatedNumber)
-                {
-                    Console.WriteLine("| * The generated number is greater than " + guessedNumber);
-                } else if (guessedNumber > generatedNumber)
-                {
-                    Console.WriteLine("| * The generated number is lower than " + guessedNumber);
-                }
-                Console.WriteLine("|------------------------------|");
-            }
-            Console.WriteLine("| Congratulations!");
-            Console.WriteLine("| You guessed the number after " + guessCounter + " attempts!");
-            Console.WriteLine("|==============================|");
+            game.Run();
         }
     }
 }
